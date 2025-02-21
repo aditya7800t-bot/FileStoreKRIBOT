19e07a6a25ec1edb63a87910dc6440420b7509f4
entrepreneurim <iamakashrabha@gmail.com>
2025-02-21 23:06:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
 
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗\n\nThis Media Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Please Forward This Media To Somewhere Else And Start Downloading There.</b>")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis Media Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Please Forward This Media To Somewhere Else And Start Downloading There.")
 
         # Schedule the file deletion
         asyncio.create_task(delete_files(madflix_msgs, client, k))
@@ -99,8 +99,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close")
+                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
         )
@@ -113,93 +113,49 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-import os, asyncio, humanize from pyrogram import Client, filters, version from pyrogram.enums import ParseMode from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated from bot import Bot from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, FILE_AUTO_DELETE from helper_func import subscribed, encode, decode, get_messages from database.database import add_user, del_user, full_userbase, present_user
+            disable_web_page_preview = True,
+            quote = True
+        )
+        return
 
-madflixofficials = FILE_AUTO_DELETE jishudeveloper = madflixofficials file_auto_delete = humanize.naturaldelta(jishudeveloper)
-
-@Bot.on_message(filters.command('start') & filters.private & subscribed) async def start_command(client: Client, message: Message): id = message.from_user.id if not await present_user(id): try: await add_user(id) except: pass text = message.text if len(text)>7: try: base64_string = text.split(" ", 1)[1] except: return string = await decode(base64_string) argument = string.split("-") if len(argument) == 3: try: start = int(int(argument[1]) / abs(client.db_channel.id)) end = int(int(argument[2]) / abs(client.db_channel.id)) except: return if start <= end: ids = range(start,end+1) else: ids = [] i = start while True: ids.append(i) i -= 1 if i < end: break elif len(argument) == 2: try: ids = [int(int(argument[1]) / abs(client.db_channel.id))] except: return temp_msg = await message.reply("Please Wait...") try: messages = await get_messages(client, ids) except: await message.reply_text("Something Went Wrong..!") return await temp_msg.delete()
-
-madflix_msgs = [] # List to keep track of sent messages
-
-    for msg in messages:
-
-        if bool(CUSTOM_CAPTION) & bool(msg.document):
-            caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
-        else:
-            caption = "" if not msg.caption else msg.caption.html
-
-        if DISABLE_CHANNEL_BUTTON:
-            reply_markup = msg.reply_markup
-        else:
-            reply_markup = None
-
-        try:
-            madflix_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-            madflix_msgs.append(madflix_msg)
-            
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            madflix_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-            madflix_msgs.append(madflix_msg)
-            
-        except:
-            pass
+    
 
 
-    k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗\n\nThis Media Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Please Forward This Media To Somewhere Else And Start Downloading There.</b>")
 
-    # Schedule the file deletion
-    asyncio.create_task(delete_files(madflix_msgs, client, k))
-
-    return
-else:
-    reply_markup = InlineKeyboardMarkup(
+    
+    
+@Bot.on_message(filters.command('start') & filters.private)
+async def not_joined(client: Client, message: Message):
+    buttons = [
         [
-            [
-                InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data = "about"),
-                InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close")
-            ]
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink)
         ]
+    ]
+    try:
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text = 'Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+            ]
+        )
+    except IndexError:
+        pass
+
+
+    await message.reply(
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
     )
-    await message.reply_text(
-        text = START_MSG.format(
-            first = message.from_user.first_name,
-            last = message.from_user.last_name,
-            username = None if not message.from_user.username else '@' + message.from_user.username,
-            mention = message.from_user.mention,
-            id = message.from_user.id
-        ),
-        reply_markup = reply_markup,
-        disable_web_page_preview = True,
-        quote = True
-    )
-    return
-
-@Bot.on_message(filters.command('start') & filters.private) async def not_joined(client: Client, message: Message): buttons = [ [ InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink) ] ] try: buttons.append( [ InlineKeyboardButton( text = 'ᴛʀʏ ᴀɢᴀɪɴ', url = f"https://t.me/{client.username}?start={message.command[1]}" ) ] ) except IndexError: pass
-
-msg = await message.reply(
-    text = FORCE_MSG.format(
-            first = message.from_user.first_name,
-            last = message.from_user.last_name,
-            username = None if not message.from_user.username else '@' + message.from_user.username,
-            mention = message.from_user.mention,
-            id = message.from_user.id
-        ),
-    reply_markup = InlineKeyboardMarkup(buttons),
-    quote = True,
-    disable_web_page_preview = True
-)
-
-# Wait for user to join
-while True:
-    await asyncio.sleep(10)  # Check every 10 seconds
-    if await subscribed(client, message):
-        try:
-            await msg.delete()  # Delete the join message
-        except:
-            pass
-        break
-
 
 
 
@@ -272,7 +228,14 @@ async def delete_files(messages, client, k):
         except Exception as e:
             print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
     # await client.send_message(messages[0].chat.id, "Your Video / File Is Successfully Deleted ✅")
-    await k.edit_text("<b> Your Media Is Successfully Deleted ✅ </b>")
+    await k.edit_text("Your Media Is Successfully Deleted ✅")
     
 
 
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
